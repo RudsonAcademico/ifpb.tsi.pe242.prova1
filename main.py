99847208e64dab3f4e96b44f92eeb49c638cf76e
RudsonAcademico <silva.rudson@academico.ifpb.edu.br>
2024-12-12 17:40:26 +0000

    primeira

--- main.py
+++ main.py
@@ -3,13 +3,39 @@ def q1():
     Dado um número inteiro x, retorne verdadeiro se x for um palíndromo, e falso caso contrário.
 
     """
-    pass
+    x=input("")
+    if x == x[::-1]:
+        print(True)
+    else:
+        print(False)
 
 
 def q2():
     """
     Dado um numeral romano, converta-o para um número inteiro.
     """
-    pass
+    s={
+        'I' : 1,
+        'V' : 5,
+        'X' : 10,
+        'L' : 50,
+        'C' : 100,
+        'D' : 500,
+        'M' : 1000,
+        }
+    resultado=0
+    romano = input('')
+    anterior = romano[0]
+    for x in romano:
+        if s.get(anterior) < s.get(x):
+            resultado-=s[anterior]*2
+            resultado+=s[x]
+        else:
+            resultado+=s[x]
+        anterior = x
+        
+            
+    print(resultado)
+
     
 
